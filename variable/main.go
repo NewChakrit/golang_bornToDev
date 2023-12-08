44251441f483c0f0f124f641e59841533f7bb51b
A657002 A657002 <a657002@AR8040657002N1.local>
2023-12-08 16:04:03 +0700

    sting int float

---
+++ variable/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var numberInt, numberInt2 int = 1000, 2000
+var msg string = "Hello"
+
+func main() {
+	// String, Int, Float 
+
+	numberFloat := 5.42
+
+	fmt.Println(numberInt)
+	fmt.Println(numberInt2)
+	fmt.Println(msg)
+
+	fmt.Println(float64(numberInt)+numberFloat)
+
+	fmt.Println(msg + "Chakrit")
+	fmt.Println("My money =", numberInt2)
+
+	//Array
+	
+}
