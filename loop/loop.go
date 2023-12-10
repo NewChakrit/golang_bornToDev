7b5f3fc68c6f673de8944ef9f764c1de50000ca1
A657002 A657002 <a657002@AR8040657002N1.local>
2023-12-10 15:15:35 +0700

    loop

---
+++ loop/loop.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+	var count = 15
+
+func main() {
+	// i := 0 
+	// for i < 10 {
+	// 	// fmt.Println("i = ", i)
+	// 	i += 1
+	// }
+
+	// for j := 0; j < count; j++ {
+	// 	fmt.Println("j =", j)
+	// }
+
+	var input string
+
+	for {
+		fmt.Scanf("%s", &input)
+		fmt.Println("input =", input)
+
+		if input == "exit" || input == "Exit" {
+			break
+		}
+	}
+}
